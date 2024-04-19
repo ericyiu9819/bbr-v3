3d63b4fdb2ad90ffa5dd88ef34f2e87e4a88c51b
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-04-19 16:55:48 +0800

    Update bbrv3.sh

--- bbrv3.sh
+++ bbrv3.sh
@@ -1,3 +1,6 @@
+#!/usr/bin/env bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
 if dpkg -l | grep -q 'linux-xanmod'; then
             while true; do
                   clear
