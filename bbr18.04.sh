82f0f19b335107f792cc26942fd0c260f8ed291c
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-06-25 17:47:21 +0800

    Update bbr18.04.sh

--- bbr18.04.sh
+++ bbr18.04.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+
+
+sh_ver="1.3.2"
+github="raw.githubusercontent.com/ericyiu9819/bbr-v3/master"
+
+Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
+Info="${Green_font_prefix}[信息]${Font_color_suffix}"
+Error="${Red_font_prefix}[错误]${Font_color_suffix}"
+Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 #安装18.04內核
   install(){
         apt-get install --install-recommends linux-generic-hwe-18.04
