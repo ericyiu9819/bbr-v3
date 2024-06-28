2e0a2cff7bad0bfabc0c0f3b997321baa4746ed0
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-06-28 22:51:42 +0800

    Update bbr18.04.sh

--- bbr18.04.sh
+++ bbr18.04.sh
@@ -2,64 +2,20 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
-
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
 #安装18.04內核
-  install(){
+  
         apt-get install --install-recommends linux-generic-hwe-18.04
-        }
+
 
 #移除舊版內核
-  romove(){
+
   apt-get update && apt-get upgrade -y && apt-get autoremove -y --purge
-        }
+  
+
 #開啟bbr
- openbbr(){
- modprobe tcp_bbr
+  modprobe tcp_bbr
 echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
-       }
-#开始菜单
-start_menu(){
-clear
-echo && echo -e " TCP加速 一键安装管理脚本 ${Red_font_prefix}[v${sh_ver}]${Font_color_suffix}
-
-  
- ${Green_font_prefix}0.${Font_color_suffix} 升级脚本
-————————————内核管理————————————
- ${Green_font_prefix}1.${Font_color_suffix} 安装BBR版18.04内核
- ${Green_font_prefix}2.${Font_color_suffix} 卸載多餘內核 
- ${Green_font_prefix}3.${Font_color_suffix} 開啟 算法
- ————————————————————————————————" && echo
-
-	check_status
-	if [[ ${kernel_status} == "noinstall" ]]; then
-		echo -e " 当前状态: ${Green_font_prefix}未安装${Font_color_suffix} 加速内核 ${Red_font_prefix}请先安装内核${Font_color_suffix}"
-	else
-		echo -e " 当前状态: ${Green_font_prefix}已安装${Font_color_suffix} ${_font_prefix}${kernel_status}${Font_color_suffix} 加速内核 , ${Green_font_prefix}${run_status}${Font_color_suffix}"
-		
-	fi
-echo
-read -p " 请输入数字 [0-11]:" num
-case "$num" in
-	1)
-	install
-	;;
-	2)
-        romove
-	3)
-        openbbr
-	;;
-        *)
-	clear
-	echo -e "${Error}:请输入正确数字 [0-14]"
-	sleep 5s
-	start_menu
-	;;
-esac
-}
+       
