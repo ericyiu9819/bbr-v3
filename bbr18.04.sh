f6dbb184c5b808e5902eca0705f886ea53c721bf
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-07-20 14:54:01 +0800

    Update bbr18.04.sh

--- bbr18.04.sh
+++ bbr18.04.sh
@@ -3,15 +3,15 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #安装18.04內核
-  install18.04{
+  install18.04{   
         apt-get install --install-recommends linux-generic-hwe-18.04
-}
+              }
 
 #移除舊版內核
  remove{
 
        apt-get update && apt-get upgrade -y && apt-get autoremove -y --purge
-}  
+       }  
 
 #開啟bbr
   modprobe tcp_bbr
