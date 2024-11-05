684af7d3b7c2d9f5eca73d8f862c068fcd193b9a
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2024-11-05 15:37:38 +0800

    Update bbr18.04.sh

--- bbr18.04.sh
+++ bbr18.04.sh
@@ -5,12 +5,12 @@ export PATH
 #安装18.04內核
   install18.04{   
         apt-get install --install-recommends linux-generic-hwe-18.04
-	}
+	      }
 
 #移除舊版內核
  remove{
         apt-get update && apt-get upgrade -y && apt-get autoremove -y --purge
-	}
+       }
 
 #開啟bbr
   modprobe tcp_bbr
@@ -18,3 +18,6 @@ echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
 echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf
 sysctl -p
+
+#重啟
+reboot
