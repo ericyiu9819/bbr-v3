b379d9872a2a27341bce7d00db82ae2ff76ece91
ericyiu9819 <38342525+ericyiu9819@users.noreply.github.com>
2025-01-19 11:21:07 +0800

    Update bbr18.04.sh

--- bbr18.04.sh
+++ bbr18.04.sh
@@ -3,16 +3,12 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
 #安装18.04內核
- install18.04{
-    apt-get install --install-recommends linux-generic-hwe-18.04
-              }
-
-#移除舊版內核
- remove{
-        apt-get update && apt-get upgrade -y && apt-get autoremove -y --purge
-       }
-
-#開啟bbr
+ apt-get install --install-recommends linux-generic-hwe-18.04
+ 
+ #移除舊版內核
+  apt-get update && apt-get upgrade -y && apt-get autoremove -y --purge
+ #開啟bbr
+  
   modprobe tcp_bbr
 echo "tcp_bbr" >> /etc/modules-load.d/modules.conf
 echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf
